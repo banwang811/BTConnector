4346d92659a28b6fdea89b50f3d3a83dabefd215
banwang811 <759919341@qq.com>
2017-12-27 11:53:06 +0800

    init

---
+++ BTConnector/BTConnector.h
@@ -0,0 +1,13 @@
+//
+//  BTConnector.h
+//  BTConnector
+//
+//  Created by banwang on 2017/12/27.
+//  Copyright © 2017年 guanqun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTConnector : NSObject
+
+@end
---
+++ BTConnector/BTConnector.m
@@ -0,0 +1,13 @@
+//
+//  BTConnector.m
+//  BTConnector
+//
+//  Created by banwang on 2017/12/27.
+//  Copyright © 2017年 guanqun. All rights reserved.
+//
+
+#import "BTConnector.h"
+
+@implementation BTConnector
+
+@end
